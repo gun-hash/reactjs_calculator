726fa931e85ef3813f18eba3e6ab145c27eeee75
gun-hash <binitme449@gmail.com>
2022-05-02 10:55:15 +0545

    done

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,13 +1,16 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Calculator } from "./calculator.component";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Home = () =>
 {
     return(
     <React.Fragment>
+    <ToastContainer position="top-center"/>
     <div className="row">
-        <h1 className="text-center mb-5 mt-5">Calculator App</h1>
+        <h1 className="text-center mt-5">Calculator App</h1>
         <div className="row">
             <div className="col-3"></div>
             <div className="mt-5 col-6 text-center">
--- src/calculator.component.js
+++ src/calculator.component.js
@@ -1,16 +1,71 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Button } from 'react-bootstrap';
 
 export const Calculator = () =>{
 
+    var calcu = {
+        a : '',
+        b : '',
+        op : '',
+        result : '',
+        isOp : false,
+        all : ''
+    }
+
     const handleClick = (ev) => {
-        let clicked = ev.target.textContent;
+        var clicked = ev.target.textContent;
+        var d1 = document.getElementById('display1');
+        var d2 = document.getElementById('display2');
+
         switch (clicked) {
             case "1":
-                
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "0":
+            case "00":
+                if(calcu.isOp===false){
+                    calcu.a += clicked;
+                    calcu.all = calcu.a + calcu.op + calcu.b;
+                    d2.innerHTML = calcu.all;
+                }
+                else if(calcu.isOp===true){
+                    calcu.b += clicked;
+                    calcu.all =  calcu.a + calcu.op + calcu.b;
+                    d2.innerHTML = calcu.all;
+                }
                 break;
-        
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                if(calcu.isOp=== false){
+                    calcu.op = clicked;
+                    calcu.all =  calcu.a + calcu.op + calcu.b;
+                    d2.innerHTML = calcu.all;
+                    calcu.isOp = true;
+                }
+                else if(calcu.isOp===true){
+                    toast('We only allow simple binary operations.', {position: "top-center"});
+                }
+                break;
+            case "Del":
+                calcu.all = calcu.all.slice(0,calcu.all.length-1);
+                d2.innerHTML = calcu.all;
+                break;
+            case "=":
+                calcu.result = eval(calcu.all);
+                d2.innerHTML = calcu.result;
+                d1.innerHTML = calcu.all;
+                break;
             default:
                 break;
         }
@@ -22,10 +77,10 @@ export const Calculator = () =>{
         <div className="row">
             <div className="col-12 calc rounded">
                 <div className="row">
-                    <div className="col-12 mt-3">
+                    <div className="col-12 mt-3 mb-2">
                         <p id='display1'></p>
                     </div>
-                    <div className="col-12 mt-3">
+                    <div className="col-12">
                         <p id='display2'></p>
                     </div>
                 </div>
@@ -41,7 +96,7 @@ export const Calculator = () =>{
                 <Button className='fullbtn' onClick={handleClick} variant="secondary">3</Button>
             </div>
             <div className="col-3">
-                <Button className='fullbtn' onClick={handleClick} variant="secondary">Add</Button>
+                <Button className='fullbtn' onClick={handleClick} variant="secondary">+</Button>
             </div>
             {/* second row */}
             <div className="col-3">
@@ -54,7 +109,7 @@ export const Calculator = () =>{
                 <Button className='fullbtn' onClick={handleClick} variant="secondary">6</Button>
             </div>
             <div className="col-3">
-                <Button className='fullbtn' onClick={handleClick} variant="secondary">Sub</Button>
+                <Button className='fullbtn' onClick={handleClick} variant="secondary">-</Button>
             </div>
             {/* third row */}
             <div className="col-3">
@@ -67,7 +122,7 @@ export const Calculator = () =>{
                 <Button className='fullbtn' onClick={handleClick} variant="secondary">9</Button>
             </div>
             <div className="col-3">
-                <Button className='fullbtn' onClick={handleClick} variant="secondary">Mul</Button>
+                <Button className='fullbtn' onClick={handleClick} variant="secondary">*</Button>
             </div>
             {/* fourth row */}
             <div className="col-3">
@@ -77,10 +132,10 @@ export const Calculator = () =>{
                 <Button className='fullbtn' onClick={handleClick} variant="secondary">0</Button>
             </div>
             <div className="col-3">
-                <Button className='fullbtn' onClick={handleClick} variant="secondary">Sqr</Button>
+                <Button className='fullbtn' onClick={handleClick} variant="secondary">Del</Button>
             </div>
             <div className="col-3">
-                <Button className='fullbtn' onClick={handleClick} variant="secondary">Div</Button>
+                <Button className='fullbtn' onClick={handleClick} variant="secondary">/</Button>
             </div>
             {/* fifth row */}
             <div className="col-12">
